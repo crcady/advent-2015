9f9054e83abce3564176ab2d00451015a2611557
Camdon Cady <camdon.cady@gmail.com>
2025-01-14 11:52:16 -0600

    formatting

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,7 +4,9 @@ fn main() {
     println!("Hello, world!");
     let mut user_input = String::new();
 
-    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
     let user_input = user_input.trim();
 
     let mut santa = build_santa();
@@ -12,14 +14,14 @@ fn main() {
     for c in user_input.chars() {
         santa.walk(c);
     }
-    
+
     println!("Santa visitied {} unique houses", santa.been.len());
 
     let mut santa = build_santa();
     let mut robot = build_santa();
 
     for (i, c) in user_input.chars().enumerate() {
-        if i % 2 == 0{
+        if i % 2 == 0 {
             santa.walk(c);
         } else {
             robot.walk(c);
@@ -28,11 +30,13 @@ fn main() {
 
     let all_locs = santa.been.union(&robot.been).collect::<Vec<&Location>>();
 
-    println!("Santa and the robot together visitied {} unique houses", all_locs.len());
-
+    println!(
+        "Santa and the robot together visitied {} unique houses",
+        all_locs.len()
+    );
 }
 
-#[derive(PartialEq,Eq, Hash, Clone, Copy)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
 struct Location {
     x: i32,
     y: i32,
@@ -44,11 +48,14 @@ struct Santa {
 }
 
 fn build_santa() -> Santa {
-    let initial_loc = Location {x: 0, y: 0};
+    let initial_loc = Location { x: 0, y: 0 };
     let mut hs: HashSet<Location> = HashSet::new();
     hs.insert(initial_loc);
 
-    Santa { loc: initial_loc, been: hs }
+    Santa {
+        loc: initial_loc,
+        been: hs,
+    }
 }
 
 impl Santa {
@@ -56,11 +63,11 @@ impl Santa {
         let x = self.loc.x;
         let y = self.loc.y;
         let new_loc = match c {
-            '>' => Location { x: x+1, y: y },
-            '<' => Location { x: x-1, y: y },
-            'v' => Location { x: x, y: y+1 },
-            '^' => Location { x: x, y: y-1},
-            _ => Location {x: x, y: y}, // Do nothing for now
+            '>' => Location { x: x + 1, y: y },
+            '<' => Location { x: x - 1, y: y },
+            'v' => Location { x: x, y: y + 1 },
+            '^' => Location { x: x, y: y - 1 },
+            _ => Location { x: x, y: y }, // Do nothing for now
         };
 
         self.been.insert(new_loc);
