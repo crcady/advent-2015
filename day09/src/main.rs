b2f6d93206f64aa20fc95bab7b444c644c920d30
Camdon Cady <camdon.cady@gmail.com>
2025-01-16 11:19:22 -0600

    Recursion is neat

---
+++ day09/src/main.rs
@@ -0,0 +1,116 @@
+use std::{
+    cmp::{max, min},
+    collections::{HashMap, HashSet},
+    io::{self, BufRead},
+};
+
+fn main() {
+    println!("Enter cities in the format\nPhoenix to Prescott = 100\nctrl-d when done");
+    let mut edges: Vec<Edge> = Vec::new();
+
+    for line in io::stdin().lock().lines() {
+        let new_edge = parse_line(&line.unwrap().trim());
+        edges.push(new_edge);
+    }
+
+    println!("Got {} edges, checking for cities", edges.len());
+
+    let mut cities: HashSet<String> = HashSet::new();
+
+    for edge in &edges {
+        cities.insert(edge.src.to_string()); // Don't want cities to own the edge
+        cities.insert(edge.dst.to_string());
+    }
+
+    println!("Found {} cities", cities.len());
+
+    let cities: Vec<String> = cities.iter().map(|x| x.to_string()).collect(); // TODO: Extra allocation?
+    let mut indices: HashMap<String, usize> = HashMap::new();
+
+    for i in 0..cities.len() {
+        indices.insert(cities[i].to_string(), i);
+    }
+
+    let mut distances: Vec<Vec<u32>> = vec![vec!(0; cities.len()); cities.len()];
+    for edge in &edges {
+        let i = *indices.get(&edge.src).unwrap();
+        let j = *indices.get(&edge.dst).unwrap();
+        distances[i][j] = edge.distance;
+        distances[j][i] = edge.distance;
+    }
+
+    let min_distance = badly_recursive_search(
+        &distances,
+        &indices.values().map(|x| *x).collect(),
+        None,
+        min,
+    );
+    let max_distance = badly_recursive_search(
+        &distances,
+        &indices.values().map(|x| *x).collect(),
+        None,
+        max,
+    );
+
+    println!("Minimum distance found: {}", min_distance);
+    println!("Maximum distance found: {}", max_distance);
+}
+
+fn badly_recursive_search(
+    distances: &Vec<Vec<u32>>,
+    remaining_cities: &Vec<usize>,
+    from: Option<usize>,
+    cmp: fn(u32, u32) -> u32,
+) -> u32 {
+    if remaining_cities.len() == 1 {
+        return distances[remaining_cities[0]][from.unwrap()]; // It would be an error to only have one city and no from
+    }
+
+    let mut min_found: Option<u32> = None;
+
+    for skip_index in 0..remaining_cities.len() {
+        let mut new_remaining: Vec<usize> = Vec::new();
+        for i in 0..remaining_cities.len() {
+            if i == skip_index {
+                continue;
+            }
+
+            new_remaining.push(remaining_cities[i]);
+        }
+        let arrival_cost: u32 = match from {
+            Some(index) => distances[index][remaining_cities[skip_index]],
+            _ => 0,
+        };
+        let found_result = badly_recursive_search(
+            distances,
+            &new_remaining,
+            Some(remaining_cities[skip_index]),
+            cmp,
+        ) + arrival_cost;
+        min_found = match min_found {
+            Some(current) => Some(cmp(current, found_result)),
+            _ => Some(found_result),
+        }
+    }
+
+    return min_found.unwrap();
+}
+
+fn parse_line(line: &str) -> Edge {
+    // The format of the line is:
+    // Source to Destination = Distance
+
+    let splits: Vec<&str> = line.split(" ").collect();
+    Edge {
+        src: splits[0].to_string(),
+        dst: splits[2].to_string(),
+        distance: splits[4].parse().expect("Couldn't parse distance"),
+    }
+}
+
+#[derive(Debug)]
+struct Edge {
+    src: String,
+    dst: String,
+    distance: u32,
+}
