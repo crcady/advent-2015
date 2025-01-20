a53da00c4bc473f65c106e0c6b909187501b7168
Camdon Cady <camdon.cady@gmail.com>
2025-01-20 11:29:00 -0600

    Day 20, part 2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -3,10 +3,10 @@ use std::collections::HashSet;
 
 fn main() {
     let mut i = 1;
-    let mut res = presents_at(i);
+    let mut res = presents_at2(i);
     while res < 36_000_000 {
         i += 1;
-        res = presents_at(i);
+        res = presents_at2(i);
     }
     println!("House {} got {} presents.", i, res);
 }
@@ -28,6 +28,29 @@ fn presents_at(house_number: u32) -> u32 {
 
     for elf in &elves {
         presents += 10*elf;
+    }
+    presents
+}
+
+fn presents_at2(house_number: u32) -> u32 {
+    let factorization = Factorization::<u32>::run(house_number);
+
+    let mut elves: HashSet<u32> = HashSet::new();
+    elves.insert(1);
+
+    for i in 0..factorization.factors.len() {
+        let old_elves: HashSet<_> = HashSet::from_iter(elves.iter().map(|x| *x));
+        for e in old_elves {
+            elves.insert(factorization.factors[i] * e);
+        }
+    }
+
+    let mut presents = 0;
+
+    for elf in &elves {
+        if house_number/elf <= 50 {
+            presents += 11*elf;
+        }
     }
     presents
 }
