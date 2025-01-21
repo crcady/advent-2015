db6329a05e6bdbbab3fcae40e4f3ec75c5c2d9dd
Camdon Cady <camdon.cady@gmail.com>
2025-01-20 22:55:08 -0600

    Day 24b, way more efficient

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -9,6 +9,76 @@ fn main() {
 
     let sum:u64 = nums.iter().sum();
 
+    if sum % 4 != 0 {
+        println!("The sum ({}) is not divisible by four!", sum);
+        panic!()
+    }
+
+    let target_value = sum / 4;
+
+    let mut combos = find_combos(&nums, target_value);
+    println!("Found {} combinations that add up to {}", combos.len(), target_value);
+    combos.sort_by_key(|x|x.len());
+    let mut viable_length: Option<usize> = None;
+    let mut min_qe: Option<u64> = None;
+
+    for i in 0..combos.len() {
+        let current = &combos[i];
+        if let Some(vl) = viable_length {
+            if current.len() > vl {
+                break;
+            }
+        }
+
+        let mut all_numbers: HashSet<u64> = HashSet::from_iter(nums.iter().map(|x|*x));
+        for num in current {
+            all_numbers.remove(num);
+        }
+
+        let remaining_numbers: Vec<u64> = Vec::from_iter(all_numbers.iter().map(|x| *x));
+        let other_combos = find_combos(&remaining_numbers, target_value);
+        if other_combos.len() == 0 {
+            continue;
+        }
+        let mut viable = false;
+        for vec in &other_combos {
+            let mut all_numbers: HashSet<u64> = HashSet::from_iter(nums.iter().map(|x|*x));
+            for num in current {
+                all_numbers.remove(num);
+            }
+
+            for num in vec {
+                all_numbers.remove(num);
+            }
+
+            let remaining_numbers: Vec<u64> = Vec::from_iter(all_numbers.iter().map(|x| *x));
+            if quick_check(&remaining_numbers, target_value) {
+                viable = true;
+                break;
+            }
+        }
+
+        if !viable {
+            continue;
+        }
+
+        // If we got here, we have a viable first group
+        viable_length = Some(current.len());
+        let new_qe: u64 = current.iter().product();
+        if let Some(qe) = min_qe {
+            if new_qe < qe {
+                min_qe = Some(new_qe);
+            }
+        } else {
+            min_qe = Some(new_qe);
+        }
+
+    }
+
+    println!("Including the trunk, the minimum QE is {}", min_qe.unwrap());
+
+
+
     if sum % 3 != 0 {
         println!("The sum ({}) is not divisible by three!", sum);
         panic!()
@@ -26,7 +96,6 @@ fn main() {
     println!("Looking at combinations of {} packages", min_packages);
 
     let mut min_qe: u64 = combos[0].iter().product();
-    let mut best_index: usize = 0;
 
     println!("The first set has a QE of {}", min_qe);
     for i in 1..combos.len() {
@@ -36,22 +105,9 @@ fn main() {
         let new_qe: u64 = combos[i].iter().product();
         if new_qe < min_qe {
             min_qe = new_qe;
-            best_index = i;
         }
     }
     println!("The minimum quantum entanglement is {}", min_qe);
-    dbg!(&combos[best_index]);
-
-    let mut all_numbers: HashSet<u64> = HashSet::from_iter(nums.iter().map(|x|*x));
-    for num in &combos[best_index] {
-        all_numbers.remove(num);
-    }
-
-    let remaining_numbers: Vec<u64> = Vec::from_iter(all_numbers.iter().map(|x| *x));
-    let mut other_combos = find_combos(&remaining_numbers, target_value);
-    other_combos.sort_by_key(|x|x.len());
-    dbg!(&other_combos[0]);
-
 
 }
 
